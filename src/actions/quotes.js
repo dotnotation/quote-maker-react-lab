56484befe3505fea1def4f9f2c23451f44fe8666
dotnotation <dotinmotionproductions@gmail.com>
2022-02-23 16:47:21 -0500

    commit tree d2280acc32dd87d87606393bd53a70d734a99c49

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,35 @@
 // TODO: Create action creators as defined in tests
+// all actions are functions that you are exporting
+// could also write all of these as arrow functions
+// export const downVote = (quoteId) => ({ type: "DOWNVOTE_QUOTE", quoteId: quoteId })
+export function addQuote(quote = {}){
+// want this to return an object with a type of "ADD_QUOTE" and a quote object
+    return{
+        type: "ADD_QUOTE",
+        quote: quote
+    }
+}
+
+export function removeQuote(quoteId){
+    // should return an object with a type of "REMOVE_QUOTE" and a quoteID
+    return{
+        type: "REMOVE_QUOTE",
+        quoteId: quoteId
+    }
+}
+
+export function upvoteQuote(quoteId){
+    // should return an object with a type of "UPVOTE_QUOTE" and a quoteId
+    return{
+        type: "UPVOTE_QUOTE",
+        quoteId: quoteId
+    }
+}
+
+export function downvoteQuote(quoteId){
+    // should return an object with a type of "DOWNVOTE_QUOTE" and a quoteId
+    return{
+        type: "DOWNVOTE_QUOTE",
+        quoteId: quoteId
+    }
+}
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,31 @@
 export default (state = [], action) => {
-  return state;
+  switch(action.type){
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+      // or return state.concat(action.quote)
+
+    case "REMOVE_QUOTE":
+      return state.filter(q => q.id !== action.quoteId)
+
+    case "UPVOTE_QUOTE":
+      let quoteIndex = state.findIndex(q => q.id === action.quoteId)
+      let quote = {...state[quoteIndex], votes: state[quoteIndex].votes + 1}
+      return [...state.slice(0, quoteIndex), quote, ...state.slice(quoteIndex + 1)]
+
+      case 'DOWNVOTE_QUOTE':
+        let index = state.findIndex(quote => quote.id === action.quoteId);
+        let quoteDown = state[index];
+        if (quoteDown.votes > 0) {
+          return [
+            ...state.slice(0, index),
+            Object.assign({}, quoteDown, { votes: quoteDown.votes -= 1 }),
+            ...state.slice(index + 1)
+          ];
+        }
+        return state;
+
+    default:
+      return state;
+
+  }
 }
